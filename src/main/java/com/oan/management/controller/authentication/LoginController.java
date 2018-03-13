a28df04d2a149f0159b693af60618acb04137014
Oan Stultjens <oan.stultjens@hotmail.com>
2018-03-13 13:58:29 +0100

    - Fixed /login still accesable after authentication

--- src/main/java/com/oan/management/controller/authentication/LoginController.java
+++ src/main/java/com/oan/management/controller/authentication/LoginController.java
@@ -1,6 +1,6 @@
 package com.oan.management.controller.authentication;
 
-import com.oan.management.model.User;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,14 +8,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 /**
  * Created by Oan on 24/01/2018.
  * @author Oan Stultjens
- * This controller only contains the GET controller to show the login pages
+ * This controller only contains the GET controller to show the login page
  */
 
 @Controller
 public class LoginController {
     @GetMapping("/login")
-    public String login(Model model, User user) {
-        model.addAttribute("page-title", "Login");
-        return "login";
+    public String login(Model model, Authentication authentication) {
+        if (authentication == null) {
+            model.addAttribute("page-title", "Login");
+            return "login";
+        } else {
+            return "redirect:/";
+        }
+
     }
 }
